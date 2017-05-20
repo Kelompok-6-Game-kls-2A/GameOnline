0a7b677ea93ee715c1bb9e14d2e60969a8116819
tari <tari@OdyDowe>
2017-05-20 17:57:19 +0700

    Dwi Lestari Mulianingsih

--- src/GME/Clan.java
+++ src/GME/Clan.java
@@ -86,3 +86,80 @@ public class Clan extends JFrame {
 	public void setMember(String member) {
 		this.member = member;
 	}
+	
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CLan frame = new CLan();					
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CLan() {
+		
+		//Clandata kclan = new Clandata();
+		//player players = new player();
+		
+			
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 998, 388);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.GREEN);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTopplyayer = new JLabel("1");
+		lblTopplyayer.setBackground(Color.ORANGE);
+		lblTopplyayer.setBounds(71, 161, 62, 14);
+		contentPane.add(lblTopplyayer);
+		
+		JLabel label = new JLabel("2");
+		label.setBackground(Color.ORANGE);
+		label.setBounds(71, 207, 62, 14);
+		contentPane.add(label);
+		
+		JLabel label_2 = new JLabel("3");
+		label_2.setBackground(Color.ORANGE);
+		label_2.setBounds(71, 251, 62, 14);
+		contentPane.add(label_2);
+		
+		JLabel lblMembers = new JLabel("Members : 10/10");
+		lblMembers.setBounds(682, 161, 111, 14);
+		contentPane.add(lblMembers);
+		
+		JLabel label_10 = new JLabel("Members : 10/10");
+		label_10.setBounds(682, 207, 111, 14);
+		contentPane.add(label_10);
+		
+		JLabel label_12 = new JLabel("Members : 10/10");
+		label_12.setBounds(682, 251, 111, 14);
+		contentPane.add(label_12);
+		
+		JLabel lblScore = new JLabel("50000");
+		lblScore.setBounds(835, 161, 46, 14);
+		contentPane.add(lblScore);
+		
+		JLabel label_15 = new JLabel("45000");
+		label_15.setBounds(835, 207, 46, 14);
+		contentPane.add(label_15);
+		
+		JLabel label_17 = new JLabel("40000");
+		label_17.setBounds(835, 251, 46, 14);
+		contentPane.add(label_17);
+		
+		
