c3354665c02ec5aee4ea25fcaea6a0417e2d55f1
Global Store <Global Store@172.16.12.60>
2017-05-20 16:43:06 +0700

    constructor

--- src/GME/Clan.java
+++ src/GME/Clan.java
@@ -1,42 +1,88 @@
 package GME;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+import java.awt.HeadlessException;
+import java.awt.LayoutManager;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+import javafx.beans.binding.StringExpression;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.awt.Font;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.SystemColor;
+
+
 
 public class Clan extends JFrame {
+	
+	protected static final String[] args = null;
+	private String namaclan ;
+	private String Lvl;
+	private String score;
+	private String jumlahMember;
+	private String member;
 
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Clan frame = new Clan();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	
+		public Clan(String namaclan, String lvl, String score, String jumlahMember, String member) throws HeadlessException {
+		super();
+		this.namaclan = namaclan;
+		this.Lvl = lvl;
+		this.score = score;
+		this.jumlahMember = jumlahMember;
+		this.member = member;
+		
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public Clan() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	public String getNamaclan() {
+		return namaclan;
 	}
 
-}
+	public void setNamaclan(String namaclan) {
+		this.namaclan = namaclan;
+	}
+
+	public String getLvl() {
+		return Lvl;
+	}
+
+	public void setLvl(String lvl) {
+		Lvl = lvl;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+	public String getJumlahMember() {
+		return jumlahMember;
+	}
+
+	public void setJumlahMember(String jumlahMember) {
+		this.jumlahMember = jumlahMember;
+	}
+	
+	public String getMember() {
+		return member;
+	}
+
+	public void setMember(String member) {
+		this.member = member;
+	}
